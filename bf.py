5fbfb725f1074b61198d2b1e18e129873cb42dc3
Nick Russo <njrusmc@gmail.com>
2024-01-05 21:35:15 +0000

    working

--- bf.py
+++ bf.py
@@ -9,8 +9,7 @@ on the OSPF routing protocol using archived configurations.
 import sys
 import json
 import pandas
-from pybatfish.client.commands import *
-from pybatfish.question import bfq, load_questions
+from pybatfish.client.session import Session
 
 # Global pandas formatting for string display
 pandas.set_option("display.width", 1000)
@@ -24,24 +23,27 @@ def main(directory):
     """
 
     # Perform basic initialization per documentation
-    bf_session.host = "localhost"
-    bf_set_network(directory)
-    bf_init_snapshot(f"snapshots/{directory}", name=directory, overwrite=True)
-    load_questions()
+    bf = Session(host="localhost")
+    bf.set_network(directory)
+    bf.init_snapshot(f"snapshots/{directory}", name=directory, overwrite=True)
 
     # Identify the questions to ask (not calling methods yet)
-    bf_questions = {
-        "proc": bfq.ospfProcessConfiguration,
-        "intf": bfq.ospfInterfaceConfiguration,
-        "area": bfq.ospfAreaConfiguration,
-        "nbrs": bfq.ospfEdges,
+    questions = {
+        # "proc": bfq.ospfProcessConfiguration,
+        # "intf": bfq.ospfInterfaceConfiguration,
+        # "area": bfq.ospfAreaConfiguration,
+        "l3if": bf.q.layer3Edges,
+        "scmp": bf.q.ospfSessionCompatibility,
+        "nbrs": bf.q.ospfEdges,
+        "rtes": bf.q.routes,
     }
 
     # Unpack dictionary tuples and iterate over them
-    for short_name, bf_question in bf_questions.items():
+    for short_name, question in questions.items():
 
         # Ask the question and store the response pandas frame
-        pandas_frame = bf_question().answer().frame()
+        pandas_frame = question().answer().frame()
+        print(f"---{short_name}---\n{pandas_frame}\n")
 
         # Assemble the generic file name prefix
         file_name = f"outputs/{short_name}_{directory}"
@@ -51,25 +53,10 @@ def main(directory):
         with open(f"{file_name}.json", "w") as handle:
             json.dump(json_data, handle, indent=2)
 
-        # Generate HTML data for web browser viewing
-        html_data = pandas_frame.to_html()
-        with open(f"{file_name}.html", "w") as handle:
-            handle.write(html_data)
-
-        # Generate CSV data using pipe separator (bf data has commas)
-        csv_data = pandas_frame.to_csv(sep="|")
-        with open(f"{file_name}.csv", "w") as handle:
-            handle.write(csv_data)
-
-        # Store string version of pandas data frame (table-like)
-        with open(f"{file_name}.pandas.txt", "w") as handle:
-            handle.write(str(pandas_frame))
 
 if __name__ == "__main__":
-    # Check for at least 2 CLI args; fail if absent
     if len(sys.argv) < 2:
-        print("usage: python bf.py <snapshot_dir_name>")
-        sys.exit(1)
+        main("subtopo")
 
     # Snapshot directory was specified; pass it into main
     else:
